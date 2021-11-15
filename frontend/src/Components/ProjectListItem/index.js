dfa269404119b3562496d61e4ea66c0b332206ce
aryelciu001 <aryelciu001@gmail.com>
2021-11-15 17:25:12 +0800

    ADD: buttons to project list item

--- frontend/src/Components/ProjectListItem/index.js
+++ frontend/src/Components/ProjectListItem/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './index.scss';
+import Button from '@mui/material/Button';
 
 export default function ProjectItemList (props) {
   const [openDesc, setOpenDesc] = useState(false);
@@ -8,13 +9,21 @@ export default function ProjectItemList (props) {
     setOpenDesc(!openDesc);
   }
 
+  const reserve = (e) => {
+    e.stopPropagation();
+  }
+
   return (
     <div 
       className="project-list-item" 
       key={props.project.id}
     >
-      <div className="project-list-item-header" onClick={()=>toggleDesc()}>
+      <div className="project-list-item-header">
         <h2>{props.project.title}</h2>
+        <div className="buttons">
+          <Button onClick={()=>toggleDesc()} variant="contained">{openDesc ? "close" : "more"}</Button>
+          <Button onClick={(e)=>reserve(e)} variant="contained">Reserve</Button>
+        </div>
       </div>
       <div className={`project-list-item-body ${openDesc ? "open" : "close"}`}>
         <p>{props.project.description}</p>
