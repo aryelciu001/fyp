bf5b078fa20df0e55de4878da1b562c2f0641c86
aryelciu001 <aryelciu001@gmail.com>
2021-12-12 18:14:04 +0800

    DOCUMENTATION

--- backend/routes/project.js
+++ backend/routes/project.js
@@ -2,12 +2,24 @@ const ProjectRouter = require('express').Router()
 const ProjectController = require('../controllers/project')
 const AuthController = require('../controllers/auth')
 
+/**
+ * @description get list of FYPs
+ */
 ProjectRouter.get('/', function (req, res) {
   ProjectController.getFyp()
     .then((fyp) => res.send(fyp))
     .catch((e) => res.status(400).send({ code: e.code }))
 })
 
+/**
+ * @description add new FYP
+ * @requestBody
+ * - projectTitle
+ * - projectId
+ * - projectInfo
+ * - supervisorName
+ * - supervisorId
+ */
 ProjectRouter.post('/', AuthController.isAdmin, function (req, res) {
   const { projectTitle, projectId, projectInfo, supervisorName, supervisorId } = req.body
   ProjectController.addFyp(projectTitle, projectId, projectInfo, supervisorName, supervisorId)
--- backend/routes/student.js
+++ backend/routes/student.js
@@ -3,12 +3,22 @@ const StudentController = require('../controllers/student')
 const AuthController = require('../controllers/auth')
 const { encrypt, passwordIsCorrect } = require('../utils/bcrypt')
 
+/**
+ * @description get list of students
+ */
 StudentRouter.get('/', async function (req, res) {
   StudentController.getStudent()
     .then((student) => res.send(student))
     .catch((e) => res.status(400).send({ code: e.code }))
 })
 
+/**
+ * @description add new student
+ * @requestBody
+ * - studentEmail
+ * - studentMatricNumber
+ * - studentPassword
+ */
 StudentRouter.post('/', AuthController.isAdmin, async function (req, res) {
   let { studentEmail, studentMatricNumber, password } = req.body
 
