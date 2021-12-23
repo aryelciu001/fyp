3b1f29156ebb5033dc5ea2f4531d5d01c7215064
aryelciu001 <aryelciu001@gmail.com>
2021-12-23 23:13:38 +0800

    FIX db init script

--- database/init.sql
+++ database/init.sql
@@ -1,19 +1,6 @@
 CREATE DATABASE IF NOT EXISTS fyp_lists_project;
 USE fyp_lists_project;
 
--- CREATE TABLE IF NOT EXISTS admin (
---   email varchar(256) NOT NULL,
---   password varchar(256),
---   PRIMARY KEY (email)
--- );
-
--- CREATE TABLE IF NOT EXISTS student (
---   email varchar(256),
---   matriculation_number varchar(20) NOT NULL,
---   password varchar(256),
---   PRIMARY KEY (matriculation_number)
--- );
-
 CREATE TABLE IF NOT EXISTS user (
   email varchar(256) NOT NULL,
   password varchar(256),
@@ -34,4 +21,4 @@ CREATE TABLE IF NOT EXISTS fyp (
   PRIMARY KEY (project_id)
 );
 
-INSERT INTO admin (email, password) VALUES ("admin@admin.com", "$2b$08$/Mkq.4iKSdqteL1CPYEBbusYvf3e7qtm.Ql3ZgBO/6.out.s4xUMq");
+INSERT INTO user (email, password) VALUES ("admin@admin.com", "$2b$08$/Mkq.4iKSdqteL1CPYEBbusYvf3e7qtm.Ql3ZgBO/6.out.s4xUMq");
