d239caf454e0685cd4c271fc4b74ccd4a760aa45
aryelciu001 <aryelciu001@gmail.com>
2022-01-12 21:53:49 +0800

    fix(be): fix sql query

--- backend/controllers/selectioninfo.js
+++ backend/controllers/selectioninfo.js
@@ -5,7 +5,7 @@ module.exports = {
   openSelection: (time) => {
     const query = `UPDATE selectioninfo
       SET selectionopen=1, selectionopentime=${time}
-      WHERE condition id=1;`
+      WHERE id=1;`
     return new Promise((resolve, reject) => {
       mysqlQuery(query)
         .then(() => resolve())
