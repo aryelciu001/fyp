1a06e9e5a1ed1a7a92b6b8beb6e0006abc059507
aryelciu001 <aryelciu001@gmail.com>
2022-01-14 17:20:15 +0800

    feat(fe): add selection reducer to store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit'
 import UserReducer from 'Reducers/user'
 import DialogFormReducer from 'Reducers/dialogform'
 import ReservationReducer from 'Reducers/reservation'
+import SelectionReducer from 'Reducers/selection'
 
 export default configureStore({
   reducer: {
     user: UserReducer,
     dialogForm: DialogFormReducer,
     reservation: ReservationReducer,
+    selection: SelectionReducer,
   },
 })
