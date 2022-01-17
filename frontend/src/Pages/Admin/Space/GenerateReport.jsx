e13f28d1a2e936c2b80749344ac0f4aa85dbff36
aryelciu001 <aryelciu001@gmail.com>
2022-01-18 00:00:32 +0800

    feat(fe): add report generator

--- frontend/src/Pages/Admin/Space/GenerateReport.jsx
+++ frontend/src/Pages/Admin/Space/GenerateReport.jsx
@@ -1,5 +1,3 @@
-/* eslint-disable */
-
 import React from 'react'
 import { Button } from '@mui/material'
 import useAxios from 'hooks/useAxios'
@@ -11,14 +9,46 @@ export default function GenerateReport() {
 
   const generateReservationReport = () => {
     request(ApiRequestType.GET_ALL_RESERVATION)
-      .then(res => console.log(res))
-      .catch(() => alert('something is wrong'))
+      .then((res) => generateReport(res.data, 'reservation'))
+      .catch((e) => alert('something is wrong'))
   }
 
   const generateSelectionReport = () => {
     request(ApiRequestType.GET_ALL_SELECTION)
-      .then(res => console.log(res))
-      .catch(() => alert('something is wrong'))
+      .then((res) => generateReport(res.data, 'selection'))
+      .catch((e) => alert('something is wrong'))
+  }
+
+  const generateReport = (data, filename) => {
+    if (!data.length) return alert('No data available')
+    const cleanData = prepareData(data)
+    popupFile(cleanData, filename)
+  }
+
+  const prepareData = (data) => {
+    let headers = data[0]
+    headers = Object.keys(headers)
+    let csvContent = ''
+    csvContent += headers + '\r\n'
+    let row
+    data.forEach((record) => {
+      row = []
+      headers.forEach((h) => {
+        row.push(record[h])
+      })
+      row = row.join(',')
+      csvContent += row + '\r\n'
+    })
+    return csvContent
+  }
+
+  const popupFile = (data, filename) => {
+    const file = document.createElement('a')
+    const blob = new Blob([data], { type: 'text/csv;charset=utf-8;' })
+    const url = URL.createObjectURL(blob)
+    file.href = url
+    file.setAttribute('download', `${filename}.csv`)
+    file.click()
   }
 
   return (
@@ -26,14 +56,14 @@ export default function GenerateReport() {
       <div className='generate-report-buttons'>
         <Button
           className='btn-primary'
-          variant="contained"
+          variant='contained'
           onClick={generateReservationReport}
         >
           Generate Reservation Report
         </Button>
         <Button
           className='btn-primary'
-          variant="contained"
+          variant='contained'
           onClick={generateSelectionReport}
         >
           Generate Selection Report
