8ff2f5965f5c97c6551ea134369511e7128705c3
aryelciu001 <aryelciu001@gmail.com>
2022-01-21 17:12:04 +0800

    chore(be): add logger

--- backend/src/utils/Error/ErrorResponse.js
+++ backend/src/utils/Error/ErrorResponse.js
@@ -1,6 +1,9 @@
+const logger = require('../logger')
 const { SERVER_ERROR } = require('./ErrorMessage')
 
 module.exports = function(error, res) {
+  logger.error(error.message)
+
   const statusCode = error.statusCode ? error.statusCode : SERVER_ERROR.statusCode
   const code = error.code ? error.code : SERVER_ERROR.code
   const message = error.message ? error.message : SERVER_ERROR.message
