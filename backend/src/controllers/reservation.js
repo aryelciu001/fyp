0e58402672690af0ab4688f079487455687ee3ff
aryelciu001 <aryelciu001@gmail.com>
2022-01-21 17:57:00 +0800

    chore(be): use async function to wrap return value in promise

--- backend/src/controllers/reservation.js
+++ backend/src/controllers/reservation.js
@@ -1,34 +1,31 @@
 const { mysqlQuery } = require('../utils/mysqlQuery')
-const MyError = require('../utils/Error/Error')
-const ErrorMessage = require('../utils/Error/ErrorMessage')
 const { defaultErrorHandler } = require('../utils/Error/ErrorHandler')
 const SqlString = require('sqlstring')
 
 class ReservationController {
   /**
+   * @description get one reservation
+   * @param {*} email 
+   * @param {*} projno 
+   * @returns 
+   */
+  getReservation = async (email, projno) => {
+    const query = SqlString.format(`SELECT * FROM reservation
+      where email=? AND projno=?;`, [email, projno])
+    const reservation = await mysqlQuery(query)
+    return reservation[0]
+  }
+  
+  /**
    * @description add reservation
    * @param {*} email
    * @param {*} projno
    */
-  addReservation = (email, projno) => {
-    return new Promise(async (resolve, reject) => {
-      try {
-        let query = SqlString.format(`SELECT * FROM reservation
-          where email=? AND projno=?;`, [email, projno])
-        const duplicateReservation = await mysqlQuery(query)
-        if (duplicateReservation.length) {
-          return reject(new MyError(ErrorMessage.ER_DUP_ENTRY))
-        }
-
-        query = SqlString.format(`INSERT INTO reservation 
-          (email, projno) 
-          VALUES (?, ?);`, [email, projno])
-        await mysqlQuery(query)
-        return resolve()
-      } catch (e) {
-        return defaultErrorHandler(e, reject)
-      }
-    })
+  addReservation = async (email, projno) => {
+    const query = SqlString.format(`INSERT INTO reservation 
+      (email, projno) 
+      VALUES (?, ?);`, [email, projno])
+    return mysqlQuery(query)
   }
 
   /**
@@ -36,14 +33,10 @@ class ReservationController {
    * @param {*} email
    * @param {*} projno
    */
-  deleteReservation = (email, projno) => {
+  deleteReservation = async (email, projno) => {
     const query = SqlString.format(`DELETE FROM reservation 
       WHERE email=? AND projno=?;`, [email, projno])
-    return new Promise((resolve, reject) => {
-      mysqlQuery(query)
-        .then(() => resolve())
-        .catch((e) => defaultErrorHandler(e, reject))
-    })
+    return mysqlQuery(query)
   }
 
   /**
@@ -51,34 +44,24 @@ class ReservationController {
    * @param {*} email
    * @returns reservations
    */
-  getUserReservation = (email) => {
+  getUserReservation = async (email) => {
+    // TODO: use join to directly populate data
     const query = SqlString.format(`SELECT * FROM reservation WHERE email=?;`, [email])
-    return new Promise((resolve, reject) => {
-      mysqlQuery(query)
-        .then((reservations) => resolve(reservations))
-        .catch((e) => defaultErrorHandler(e, reject))
-    })
+    return mysqlQuery(query)
   }
 
   /**
    * @description return a table to be used for report
    * @returns query
    */
-  getReservationReportData = () => {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const query = `SELECT u.email as student_email, u.matriculation_number, p.projno, p.supervisor, p.email as supervisor_email, p.title
-          FROM reservation as r
-          JOIN user as u
-          ON r.email = u.email
-          JOIN project as p
-          ON r.projno = p.projno;`
-        const data = await mysqlQuery(query)
-        return resolve(data)
-      } catch (e) {
-        return defaultErrorHandler(e, reject)
-      }
-    })
+  getReservationReportData = async () => {
+    const query = `SELECT u.email as student_email, u.matriculation_number, p.projno, p.supervisor, p.email as supervisor_email, p.title
+      FROM reservation as r
+      JOIN user as u
+      ON r.email = u.email
+      JOIN project as p
+      ON r.projno = p.projno;`
+    return mysqlQuery(query)
   }
 }
 
